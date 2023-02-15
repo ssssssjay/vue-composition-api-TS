eb76c19b26b5ebfc9c6dd0c114e7cd1c4720a555
Sungjae.Kwak <sungjae8970@gmail.com>
2023-02-15 16:07:42 +0900

    using TS in Vue(composition api) - very easy

--- README.md
+++ README.md
@@ -1,24 +1,52 @@
 # vue3-type
 
-## Project setup
-```
-npm install
+## types folder
+
+types폴더 안에 타입들을 밀어넣고 익스포트해서 필요한 SFC에서 끌어다가 쓴다
+
+## ref & Type
+
+```jsx
+import Job from "./types/Job";
+import OrderTerm from "./types/OrderTerm";
+
+const jobList = ref<Job[]>([]);
+const order = ref<OrderTerm>("title");
 ```
 
-### Compiles and hot-reloads for development
-```
-npm run serve
+## 4-3. props & Type
+
+PropType으로 타입을 던져주자
+
+props의 type은 Vue의 스펙일뿐이다
+
+```jsx
+import { defineComponent, PropType, computed } from "vue";
+import Job from "@/types/Job";
+import OrderTerm from "@/types/OrderTerm";
+
+export default defineComponent({
+  props: {
+    jobs: {
+      required: true,
+      type: Array as PropType<Job[]>, // 이건 그냥 뷰의 스펙일뿐 타입스크립트 스펙?이 아니다
+    },
+    order: {
+      required: true,
+      type: String as PropType<OrderTerm>,
+    },
+  },
+  setup(props) {
+    const orderedJobs = computed(() => {
+      return [...props.jobs].sort((a: Job, b: Job) => {
+        return a[props.order] > b[props.order] ? 1 : -1;
+      });
+    });
+    return { orderedJobs };
+  },
+});
 ```
 
-### Compiles and minifies for production
-```
-npm run build
-```
+## 4-4. TS의 타입추론을 믿자
 
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+간단한 타입까지 선언하지는 말자
--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,71 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="app">
+    <header>
+      <div class="order">
+        <button @click="handleClick('title')">order by title</button>
+        <button @click="handleClick('salary')">order by salary</button>
+        <button @click="handleClick('location')">order by location</button>
+      </div>
+    </header>
+    <JobList :jobs="jobs" :order="order"></JobList>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent, ref } from "vue";
+import Job from "./types/Job";
+import OrderTerm from "./types/OrderTerm";
+import JobList from "./components/JobList.vue";
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "App",
+  components: { JobList },
+  setup() {
+    const jobs = ref<Job[]>([
+      { title: "FE", location: "seoul", salary: 1000000, id: "1" },
+      { title: "BE", location: "jeju", salary: 2000000, id: "2" },
+      { title: "Fullstack", location: "yongin", salary: 3000000, id: "3" },
+      { title: "DBA", location: "seoul", salary: 4000000, id: "4" },
+      { title: "PM", location: "incheon", salary: 5000000, id: "5" },
+    ]);
+
+    const order = ref<OrderTerm>("title");
+
+    const handleClick = (term: OrderTerm) => {
+      order.value = term;
+    };
+
+    return { jobs, handleClick, order };
+  },
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+header {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+header .order {
+  margin-top: 20px;
+}
+button {
+  margin: 0 10px;
+  color: #1195c9;
+  border: 3px solid #1195c9;
+  background: #d5f0ff;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+}
+header .title {
+  display: flex;
+  justify-content: center;
+}
+header img {
+  width: 60px;
+  margin-right: 20px;
+}
+header h1 {
+  font-size: 3em;
 }
 </style>
